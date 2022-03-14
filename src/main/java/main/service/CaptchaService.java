3c5dbf7cadaaa76280c24f60f49b0aa51ccd3a81
PANDA <may.co@mail.ru>
2022-03-14 13:06:56 +0500

    изменена настройка каптчи, добавлен тест

--- src/main/java/main/service/CaptchaService.java
+++ src/main/java/main/service/CaptchaService.java
@@ -16,29 +16,43 @@ import org.springframework.stereotype.Service;
 import java.awt.*;
 import java.util.Base64;
 import java.util.Date;
+import java.util.Random;
 import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class CaptchaService {
     private final long HOUR = 3_600_000;
+    private final int CAPTCHA_LEN = 4;
     CaptchaRepository captchaRepository;
 
     public CaptchaResponse getCaptcha() {
         String secret = UUID.randomUUID().toString();
         Cage cage1 = new GCage();
 
-        String code = cage1.getTokenGenerator().next();
+        String code = genString(CAPTCHA_LEN);
+
         String image = "data:image/png;base64, " + Base64.getEncoder().encodeToString(cage1.draw(code));
         CaptchaCode captchaCode = new CaptchaCode();
         captchaCode.setSecretCode(secret);
         captchaCode.setCode(code);
         captchaCode.setTime(new Date());
         captchaRepository.save(captchaCode);
-        return new CaptchaResponse(secret,image);
+        return new CaptchaResponse(secret, image);
     }
+
+    private String genString(int len) {
+        char[] letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        String captcha = "";
+        for (int i = 0; i < len; i++) {
+            int r = new Random().nextInt(10);
+            captcha += String.valueOf(letters[r]);
+        }
+        return captcha;
+    }
+
     @Scheduled(fixedRate = 60000)
-    public void deleteOldCaptcha(){
+    public void deleteOldCaptcha() {
         captchaRepository.deleteAll(captchaRepository.findOld(HOUR));
 
     }
